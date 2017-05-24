c0d10499f7e83b99044308c88e783af3a8d203bf
neeraj24a <neeraj24a@gmail.com>
2017-05-24 14:06:08 +0530

    Product Grid

--- protected/modules/admin/views/product/admin.php
+++ protected/modules/admin/views/product/admin.php
@@ -1,69 +1,109 @@
-<?php
-/* @var $this ProductController */
-/* @var $model Product */
+<section class="content-header">
+    <h1>
+        Manage
+        <small>Products</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="<?php echo base_url() . '/admin/product'; ?>"><i class="fa fa-dashboard"></i> Products</a></li>
+        <li class="active">Manage</li>
+    </ol>
+</section>
+<section class="content">
 
-$this->breadcrumbs=array(
-	'Products'=>array('index'),
-	'Manage',
-);
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Search</h3>
+                </div>
+                <?php
+                Yii::app()->clientScript->registerScript('search', "
+						$('form.search-form').submit(function(){
+							$('#product-grid').yiiGridView('update', {
+								data: $(this).serialize()
+							});
+							return false;
+						});
+						");
+                $this->renderPartial('_search', array(
+                    'model' => $model,
+                ));
+                ?>
+            </div>
+            <div class="box">
+                <div class="box-header">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h3 class="box-title">Product List</h3>
+                        </div>
+                        <div class="col-sm-4">
+                            <a href="<?php echo base_url() . '/admin/product/create'; ?>">
+                                <button class="btn btn-block btn-primary">Add Product</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-body">
+                    <div class="dataTables_wrapper form-inline dt-bootstrap">
 
-$this->menu=array(
-	array('label'=>'List Product', 'url'=>array('index')),
-	array('label'=>'Create Product', 'url'=>array('create')),
-);
-
-Yii::app()->clientScript->registerScript('search', "
-$('.search-button').click(function(){
-	$('.search-form').toggle();
-	return false;
-});
-$('.search-form form').submit(function(){
-	$('#product-grid').yiiGridView('update', {
-		data: $(this).serialize()
-	});
-	return false;
-});
-");
-?>
-
-<h1>Manage Products</h1>
-
-<p>
-You may optionally enter a comparison operator (<b>&lt;</b>, <b>&lt;=</b>, <b>&gt;</b>, <b>&gt;=</b>, <b>&lt;&gt;</b>
-or <b>=</b>) at the beginning of each of your search values to specify how the comparison should be done.
-</p>
-
-<?php echo CHtml::link('Advanced Search','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
-
-<?php $this->widget('zii.widgets.grid.CGridView', array(
-	'id'=>'product-grid',
-	'dataProvider'=>$model->search(),
-	'filter'=>$model,
-	'columns'=>array(
-		'id',
-		'sku',
-		'name',
-		'category',
-		'slug',
-		'quantity',
-		/*
-		'short_desc',
-		'description',
-		'in_stock',
-		'status',
-		'deleted',
-		'created_by',
-		'modified_by',
-		'date_entered',
-		'date_modified',
-		*/
-		array(
-			'class'=>'CButtonColumn',
-		),
-	),
-)); ?>
+                        <div class="row">
+                            <div class="col-sm-12 table-responsive">
+                                <?php
+                                $this->widget('zii.widgets.grid.CGridView', array(
+                                    'id' => 'product-grid',
+                                    'itemsCssClass' => 'table table-bordered table-hover dataTable',
+                                    'dataProvider' => $model->search(),
+                                    'enablePagination' => true,
+                                    // 'filter'=>$model,
+                                    'columns' => array(
+                                        'sku',
+										'name',
+										'category',
+                                        array(
+                                            'class'=>'CButtonColumn',
+                                            'template'=>'{r} {g} {v} {u} {d}', // <-- TEMPLATE WITH THE TWO STATES
+                                            'htmlOptions'=>array(
+                                                    'width'=>80,
+                                            ),
+                                            'buttons' => array(
+                                                'r'=>array(
+                                                        'label'=>'<i class="fa fa-search"></i>',
+                                                        'url'=>'Yii::app()->createUrl("admin/relatedProduct/view", array("product"=>$data->id))',
+                                                        'options'=>array('class'=>'view','title'=>'View'),
+                                                ),
+												'g'=>array(
+                                                        'label'=>'<i class="fa fa-search"></i>',
+                                                        'url'=>'Yii::app()->createUrl("admin/productGallery/view", array("id"=>$data->id))',
+                                                        'options'=>array('class'=>'view','title'=>'View'),
+                                                ),
+												'v'=>array(
+                                                        'label'=>'<i class="fa fa-search"></i>',
+                                                        'url'=>'Yii::app()->createUrl("admin/product/view", array("id"=>$data->id))',
+                                                        'options'=>array('class'=>'view','title'=>'View'),
+                                                ),
+                                                'u'=>array(
+                                                        'label'=>'<i class="fa fa-edit"></i>',
+                                                        'url'=>'Yii::app()->createUrl("admin/product/update", array("id"=>$data->id))',
+                                                        'options'=>array('class'=>'edit','title'=>'Update'),
+                                                ),
+                                                'd'=>array(
+                                                        'label'=>'<i class="fa fa-trash"></i>',
+                                                        'url'=>'Yii::app()->createUrl("admin/product/delete", array("id"=>$data->id))',
+                                                        'options'=>array('class'=>'delete','title'=>'Delete'),
+                                                        'click'=>'function(){return confirm("are you sure ?");}'
+                                                ),
+                                            ),
+                                        )
+                                    ),
+                                ));
+                                ?>
+                            </div>
+                        </div>
+                        <div class="row"><div class="col-sm-6"></div><div class="col-sm-6"></div></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
