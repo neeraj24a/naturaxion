d90df8a3033aa1d4747279f909c5253f1bb292db
neeraj24a <neeraj24a@gmail.com>
2017-05-24 17:17:06 +0530

    Product Create

--- protected/modules/admin/views/product/create.php
+++ protected/modules/admin/views/product/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this ProductController */
-/* @var $model Product */
-
-$this->breadcrumbs=array(
-	'Products'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List Product', 'url'=>array('index')),
-	array('label'=>'Manage Product', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create Product</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+    <h1>
+        Add
+        <small>Product</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="<?php echo base_url() . '/admin/product'; ?>"><i class="fa fa-dashboard"></i> Products</a></li>
+        <li class="active">Add</li>
+    </ol>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Product</h3>
+                </div>
+                <?php $this->renderPartial('_form', array('model' => $model)); ?>
+            </div>
+        </div>
+    </div>
+</section>
