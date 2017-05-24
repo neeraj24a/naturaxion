0d33674381eb5ff5a5669e82b61bf4f7bed975a6
neeraj24a <neeraj24a@gmail.com>
2017-05-24 17:03:35 +0530

    Update _search.php

--- protected/modules/admin/views/product/_search.php
+++ protected/modules/admin/views/product/_search.php
@@ -1,95 +1,30 @@
-<?php
-/* @var $this ProductController */
-/* @var $model Product */
-/* @var $form CActiveForm */
-?>
-
-<div class="wide form">
-
 <?php $form=$this->beginWidget('CActiveForm', array(
 	'action'=>Yii::app()->createUrl($this->route),
 	'method'=>'get',
+	'htmlOptions'=>array('class' => 'search-form')
 )); ?>
-
-	<div class="row">
-		<?php echo $form->label($model,'id'); ?>
-		<?php echo $form->textField($model,'id',array('size'=>36,'maxlength'=>36)); ?>
+<div class="box-body">
+	<div class="form-group">
+		<div class="col-xs-4">
+			<?php echo $form->label($model,'name'); ?>
+			<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>255,'class' => 'form-control')); ?>
+		</div>
+		<div class="col-xs-4">
+			<?php echo $form->label($model,'sku'); ?>
+			<?php echo $form->textField($model,'sku',array('size'=>60,'maxlength'=>255,'class' => 'form-control')); ?>
+		</div>
+		<div class="col-xs-4">
+			<?php echo $form->label($model,'category'); ?>
+			<?php 
+				$all_cat = Category:model()->findAll(); 
+				$list = CHtml::listData($all_cat, 'id', 'name');  
+			?>
+			<?php echo $form->dropDownList($model,'category',$list, array('empty'=>'Select Category','class' => 'form-control')); ?>
+		</div>
 	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'sku'); ?>
-		<?php echo $form->textField($model,'sku',array('size'=>60,'maxlength'=>64)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'name'); ?>
-		<?php echo $form->textField($model,'name',array('size'=>60,'maxlength'=>256)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'category'); ?>
-		<?php echo $form->textField($model,'category',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'slug'); ?>
-		<?php echo $form->textField($model,'slug',array('size'=>60,'maxlength'=>512)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'quantity'); ?>
-		<?php echo $form->textField($model,'quantity'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'short_desc'); ?>
-		<?php echo $form->textField($model,'short_desc',array('size'=>60,'maxlength'=>512)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'description'); ?>
-		<?php echo $form->textArea($model,'description',array('rows'=>6, 'cols'=>50)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'in_stock'); ?>
-		<?php echo $form->textField($model,'in_stock'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'status'); ?>
-		<?php echo $form->textField($model,'status'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'deleted'); ?>
-		<?php echo $form->textField($model,'deleted'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'created_by'); ?>
-		<?php echo $form->textField($model,'created_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'modified_by'); ?>
-		<?php echo $form->textField($model,'modified_by',array('size'=>36,'maxlength'=>36)); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_entered'); ?>
-		<?php echo $form->textField($model,'date_entered'); ?>
-	</div>
-
-	<div class="row">
-		<?php echo $form->label($model,'date_modified'); ?>
-		<?php echo $form->textField($model,'date_modified'); ?>
-	</div>
-
-	<div class="row buttons">
-		<?php echo CHtml::submitButton('Search'); ?>
-	</div>
-
+</div>
+<div class="box-footer">
+	<?php echo CHtml::submitButton('Search',array("class" => 'btn btn-info search-button')); ?>
+	<a href="<?php echo base_url().'/admin/product/manage' ?>" class="btn btn-warning">Clear</a>
+</div>
 <?php $this->endWidget(); ?>
-
-</div><!-- search-form -->
