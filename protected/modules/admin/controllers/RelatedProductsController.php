9f16bbae916a732fa981cbf64493ea71e8be35d5
neeraj24a <neeraj24a@gmail.com>
2017-05-25 13:06:38 +0530

    Related Products Controller Update

--- protected/modules/admin/controllers/RelatedProductsController.php
+++ protected/modules/admin/controllers/RelatedProductsController.php
@@ -11,38 +11,16 @@ class RelatedProductsController extends Controller
 	/**
 	 * @return array action filters
 	 */
-	public function filters()
+	protected function beforeAction($action)
 	{
-		return array(
-			'accessControl', // perform access control for CRUD operations
-			'postOnly + delete', // we only allow deletion via POST request
-		);
-	}
-
-	/**
-	 * Specifies the access control rules.
-	 * This method is used by the 'accessControl' filter.
-	 * @return array access control rules
-	 */
-	public function accessRules()
-	{
-		return array(
-			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('index','view'),
-				'users'=>array('*'),
-			),
-			array('allow', // allow authenticated user to perform 'create' and 'update' actions
-				'actions'=>array('create','update'),
-				'users'=>array('@'),
-			),
-			array('allow', // allow admin user to perform 'admin' and 'delete' actions
-				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
-			),
-			array('deny',  // deny all users
-				'users'=>array('*'),
-			),
-		);
+		if (!parent::beforeAction($action)) {
+	        return false;
+	    }
+		if(isFrontUserLoggedIn()){
+			return true;
+		} else {
+			$this->redirect(CController::createUrl("/admin/login"));
+		}
 	}
 
 	/**
@@ -60,7 +38,7 @@ class RelatedProductsController extends Controller
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
 	 */
-	public function actionCreate()
+	public function actionCreate($id = null)
 	{
 		$model=new RelatedProducts;
 
@@ -76,6 +54,7 @@ class RelatedProductsController extends Controller
 
 		$this->render('create',array(
 			'model'=>$model,
+			'product'=>$id
 		));
 	}
 
@@ -86,7 +65,12 @@ class RelatedProductsController extends Controller
 	 */
 	public function actionUpdate($id)
 	{
-		$model=$this->loadModel($id);
+		$p = Product::model->findByPk($id);
+		if($p === null){
+			$model=$this->loadModel($id);
+		} else {
+			$products = ProductGallery::model()->findAll(array("condition" => "product = '".$id."'"));
+		}
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
@@ -98,9 +82,15 @@ class RelatedProductsController extends Controller
 				$this->redirect(array('view','id'=>$model->id));
 		}
 
-		$this->render('update',array(
-			'model'=>$model,
-		));
+		if($p === null){
+			$this->render('update',array(
+				'model'=>$model,
+			));
+		} else {
+			$this->render('related-update',array(
+				'products'=>$products,
+			));
+		}
 	}
 
 	/**
@@ -114,7 +104,7 @@ class RelatedProductsController extends Controller
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('manage'));
 	}
 
 	/**
@@ -122,16 +112,13 @@ class RelatedProductsController extends Controller
 	 */
 	public function actionIndex()
 	{
-		$dataProvider=new CActiveDataProvider('RelatedProducts');
-		$this->render('index',array(
-			'dataProvider'=>$dataProvider,
-		));
+		$this->redirect(array("manage"));
 	}
 
 	/**
 	 * Manages all models.
 	 */
-	public function actionAdmin()
+	public function actionManage()
 	{
 		$model=new RelatedProducts('search');
 		$model->unsetAttributes();  // clear any default values
