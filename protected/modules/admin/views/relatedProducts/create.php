c1e7adb66ac63b4221fc36c1c0157b4ff23bd8ec
neeraj24a <neeraj24a@gmail.com>
2017-05-26 13:41:07 +0530

    Update Related Product create

--- protected/modules/admin/views/relatedProducts/create.php
+++ protected/modules/admin/views/relatedProducts/create.php
@@ -1,18 +1,23 @@
-<?php
-/* @var $this RelatedProductsController */
-/* @var $model RelatedProducts */
-
-$this->breadcrumbs=array(
-	'Related Products'=>array('index'),
-	'Create',
-);
-
-$this->menu=array(
-	array('label'=>'List RelatedProducts', 'url'=>array('index')),
-	array('label'=>'Manage RelatedProducts', 'url'=>array('admin')),
-);
-?>
-
-<h1>Create RelatedProducts</h1>
-
-<?php $this->renderPartial('_form', array('model'=>$model)); ?>
+<section class="content-header">
+    <h1>
+        Add
+        <small>Related Product</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="<?php echo base_url() . '/admin/dashboard'; ?>"><i class="fa fa-dashboard"></i> Dashboard</a></li>
+        <li><a href="<?php echo base_url() . '/admin/relatedProducts'; ?>"><i class="fa fa-dashboard"></i> Related Products</a></li>
+        <li class="active">Add</li>
+    </ol>
+</section>
+<section class="content">
+    <div class="row">
+        <div class="col-xs-12">
+            <div class="box box-info">
+                <div class="box-header with-border">
+                    <h3 class="box-title">Related Products</h3>
+                </div>
+                <?php $this->renderPartial('_form', array('model' => $model,'product' => $product)); ?>
+            </div>
+        </div>
+    </div>
+</section>
